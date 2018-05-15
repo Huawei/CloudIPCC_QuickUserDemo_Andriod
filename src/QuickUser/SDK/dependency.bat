5d037ac96d9652a964fdb738d0768894f0f79de2
linbin <30925551+HWlinbin@users.noreply.github.com>
2018-05-15 10:16:05 +0800

    Update dependency.bat

---
+++ src/QuickUser/SDK/dependency.bat
@@ -0,0 +1,54 @@
+mkdir ..\app\libs\armeabi
+
+
+xcopy eSDK_EC_API_V6.1.RC1_android\libs\HME-Audio.jar ..\app\libs
+xcopy eSDK_EC_API_V6.1.RC1_android\libs\HME-Video.jar ..\app\libs
+xcopy eSDK_EC_API_V6.1.RC1_android\libs\KMC.jar ..\app\libs
+xcopy eSDK_EC_API_V6.1.RC1_android\libs\TupCall.jar ..\app\libs
+xcopy eSDK_EC_API_V6.1.RC1_android\libs\TupCmptService.jar ..\app\libs
+xcopy eSDK_EC_API_V6.1.RC1_android\libs\TupConf.jar ..\app\libs
+xcopy eSDK_EC_API_V6.1.RC1_android\libs\TupRsa.jar ..\app\libs
+xcopy eSDK_EC_API_V6.1.RC1_android\libs\TupService.jar ..\app\libs
+xcopy eSDK_EC_API_V6.1.RC1_android\libs\TupSocket.jar ..\app\libs
+
+xcopy eSDK_EC_API_V6.1.RC1_android\libs\armeabi\lib_iomxcodec_40.so ..\app\libs\armeabi
+xcopy eSDK_EC_API_V6.1.RC1_android\libs\armeabi\lib_iomxcodec_42.so ..\app\libs\armeabi
+xcopy eSDK_EC_API_V6.1.RC1_android\libs\armeabi\lib_iomxcodec_43.so ..\app\libs\armeabi
+xcopy eSDK_EC_API_V6.1.RC1_android\libs\armeabi\libACE.so ..\app\libs\armeabi
+xcopy eSDK_EC_API_V6.1.RC1_android\libs\armeabi\libgnustl_shared.so ..\app\libs\armeabi
+xcopy eSDK_EC_API_V6.1.RC1_android\libs\armeabi\libHME-Audio.so ..\app\libs\armeabi
+xcopy eSDK_EC_API_V6.1.RC1_android\libs\armeabi\libHME-Video.so ..\app\libs\armeabi
+xcopy eSDK_EC_API_V6.1.RC1_android\libs\armeabi\libHWCodec_44.so ..\app\libs\armeabi
+xcopy eSDK_EC_API_V6.1.RC1_android\libs\armeabi\libHWCodec_50.so ..\app\libs\armeabi
+xcopy eSDK_EC_API_V6.1.RC1_android\libs\armeabi\libHWCodec_60.so ..\app\libs\armeabi
+xcopy eSDK_EC_API_V6.1.RC1_android\libs\armeabi\libipsi_crypto.so ..\app\libs\armeabi
+xcopy eSDK_EC_API_V6.1.RC1_android\libs\armeabi\libipsi_osal.so ..\app\libs\armeabi
+xcopy eSDK_EC_API_V6.1.RC1_android\libs\armeabi\libipsi_pse.so ..\app\libs\armeabi
+xcopy eSDK_EC_API_V6.1.RC1_android\libs\armeabi\libipsi_ssl.so ..\app\libs\armeabi
+xcopy eSDK_EC_API_V6.1.RC1_android\libs\armeabi\libKMC.so ..\app\libs\armeabi
+xcopy eSDK_EC_API_V6.1.RC1_android\libs\armeabi\libsecurec.so ..\app\libs\armeabi
+xcopy eSDK_EC_API_V6.1.RC1_android\libs\armeabi\libtscsvn.so ..\app\libs\armeabi
+xcopy eSDK_EC_API_V6.1.RC1_android\libs\armeabi\libtup_call_audio.so ..\app\libs\armeabi
+xcopy eSDK_EC_API_V6.1.RC1_android\libs\armeabi\libtup_call_bfcp.so ..\app\libs\armeabi
+xcopy eSDK_EC_API_V6.1.RC1_android\libs\armeabi\libtup_call_mediaservice.so ..\app\libs\armeabi
+xcopy eSDK_EC_API_V6.1.RC1_android\libs\armeabi\libtup_call_service.so ..\app\libs\armeabi
+xcopy eSDK_EC_API_V6.1.RC1_android\libs\armeabi\libtup_call_sip.so ..\app\libs\armeabi
+xcopy eSDK_EC_API_V6.1.RC1_android\libs\armeabi\libtup_call_video.so ..\app\libs\armeabi
+xcopy eSDK_EC_API_V6.1.RC1_android\libs\armeabi\libtup_cmpt_service.so ..\app\libs\armeabi
+xcopy eSDK_EC_API_V6.1.RC1_android\libs\armeabi\libtup_commonlib.so ..\app\libs\armeabi
+xcopy eSDK_EC_API_V6.1.RC1_android\libs\armeabi\libtup_dns.so ..\app\libs\armeabi
+xcopy eSDK_EC_API_V6.1.RC1_android\libs\armeabi\libtup_exception.so ..\app\libs\armeabi
+xcopy eSDK_EC_API_V6.1.RC1_android\libs\armeabi\libtup_ipdetect.so ..\app\libs\armeabi
+xcopy eSDK_EC_API_V6.1.RC1_android\libs\armeabi\libtup_logone.so ..\app\libs\armeabi
+xcopy eSDK_EC_API_V6.1.RC1_android\libs\armeabi\libtup_msg.so ..\app\libs\armeabi
+xcopy eSDK_EC_API_V6.1.RC1_android\libs\armeabi\libtup_os_adapter.so ..\app\libs\armeabi
+xcopy eSDK_EC_API_V6.1.RC1_android\libs\armeabi\libtup_publiclib.so ..\app\libs\armeabi
+xcopy eSDK_EC_API_V6.1.RC1_android\libs\armeabi\libtup_rsa_encrypt.so ..\app\libs\armeabi
+xcopy eSDK_EC_API_V6.1.RC1_android\libs\armeabi\libtup_socket.so ..\app\libs\armeabi
+xcopy eSDK_EC_API_V6.1.RC1_android\libs\armeabi\libtup_socket_stg.so ..\app\libs\armeabi
+xcopy eSDK_EC_API_V6.1.RC1_android\libs\armeabi\libtup_support.so ..\app\libs\armeabi
+xcopy eSDK_EC_API_V6.1.RC1_android\libs\armeabi\libtup_xml.so ..\app\libs\armeabi
+xcopy eSDK_EC_API_V6.1.RC1_android\libs\armeabi\libTupConf.so ..\app\libs\armeabi
+xcopy eSDK_EC_API_V6.1.RC1_android\libs\armeabi\libuspsdk.so ..\app\libs\armeabi
+
+
